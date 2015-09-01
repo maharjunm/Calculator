b3260499d4bf8545482731261d0946832e267eb9
maharjun <maharjun@thoughtworks.com>
2015-09-01 22:53:03 +0530

    Added spec for addition method

--- src/com/twu/calculator/Adder.java
+++ src/com/twu/calculator/Adder.java
@@ -2,11 +2,12 @@ package com.twu.calculator;
 
 public class Adder {
     private double operand;
+
     public Adder(double operand) {
         this.operand = operand;
     }
 
     public double performOperation(double accumulator) {
-        return 0;
+        return accumulator + operand;
     }
 }
--- test/com/twu/calculator/AdderTest.java
+++ test/com/twu/calculator/AdderTest.java
@@ -7,9 +7,16 @@ import static org.junit.Assert.*;
 public class AdderTest {
 
     @Test
-    public void shouldReturnZeroWhenWeAddWIthZero() {
+    public void shouldReturnZeroWhenWeAddWithZero() {
         Adder adder = new Adder(0.0);
 
         assertEquals(0.0, adder.performOperation(0.0), 0.2d);
+    }
+
+    @Test
+    public void shouldReturnTheResultWhenWeAddWithPreInitiatedValue() {
+        Adder adder = new Adder(2.0);
+
+        assertEquals(4.0, adder.performOperation(2.0), 0.2d);
     }
 }
