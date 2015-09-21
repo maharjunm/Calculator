9fdebd19c0de9e214eae4c617096ce7fe80824ce
maharjun <maharjun@thoughtworks.com>
2015-09-21 17:36:44 +0530

    Calculator should have history in it and Added mockito files to the library

--- src/com/twu/calculator/Calculator.java
+++ src/com/twu/calculator/Calculator.java
@@ -1,8 +1,11 @@
 package com.twu.calculator;
 
+import java.util.ArrayList;
+
 public class Calculator {
 
     private double accumulator;
+    History history = new History();
 
     public Calculator(double initialValue) {
         this.accumulator = initialValue;
@@ -10,6 +13,7 @@ public class Calculator {
 
     public void performProcess(Operation operation) {
         this.accumulator = operation.performOperation(accumulator);
+        history.add(operation);
     }
 
     public double getResult() {
