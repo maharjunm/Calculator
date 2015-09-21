3a21d22deb79f7d0c1aa12e4675e8d356c0de4c5
maharjun <maharjun@thoughtworks.com>
2015-09-21 16:52:33 +0530

    Calculator should return the accumulator value

---
+++ test/com/twu/calculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.twu.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void shouldReturnTheAccumulatorValue() {
+        Calculator calculator = new Calculator(0.0);
+
+        assertEquals(0.0, calculator.getResult(), 0.00d);
+    }
+}
